1ccfbdc21872c1f0c655aa9e2144042110c5d810
eden <edenv30@gmail.com>
2020-09-29 12:43:43 +0300

    Updates

---
+++ precache-manifest.a1aebe808f3ba75e808c720c6929a476.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f74efa65b6f03f6f64b7b9751ba1832b",
+    "url": "/movies-subscriptions/index.html"
+  },
+  {
+    "revision": "0ffbf250416481eef957",
+    "url": "/movies-subscriptions/static/css/main.b862b93d.chunk.css"
+  },
+  {
+    "revision": "307775666d400a065132",
+    "url": "/movies-subscriptions/static/js/2.1ee4a9bc.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/movies-subscriptions/static/js/2.1ee4a9bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ffbf250416481eef957",
+    "url": "/movies-subscriptions/static/js/main.88bbbee3.chunk.js"
+  },
+  {
+    "revision": "455a474b68e0961409c0",
+    "url": "/movies-subscriptions/static/js/runtime-main.16cdb88d.js"
+  },
+  {
+    "revision": "e271afa7e3e03a49e12a7e0768333a85",
+    "url": "/movies-subscriptions/static/media/Film.e271afa7.jpeg"
+  }
+]);
