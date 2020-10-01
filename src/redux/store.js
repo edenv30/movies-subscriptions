f5629a5779b2067d00ac559e9e6bf0f559753c89
eden <edenv30@gmail.com>
2020-10-01 15:37:48 +0300

    logger redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
-// import thunk from 'redux-thunk';
-
-
-// if (process.env.NODE_ENV === 'development') {
-//     push(logger);
-// } 
+import thunk from 'redux-thunk';
 
 const middlewares = [logger];
+
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+} 
 
 export const store = createStore(rootReducer,  applyMiddleware(...middlewares));
 // export const store = createStore(rootReducer,  applyMiddleware(thunk));
